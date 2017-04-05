ac3d7fd0381bf1e240e597d89df17dc0c5a3b37e
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2017-04-05 20:40:56 +0100

    most recent time

--- app/models/event.js
+++ app/models/event.js
@@ -1,9 +1,15 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
 
   title: DS.attr('string'),
 
-  lastTimes: DS.attr()
+  lastTimes: DS.attr(),
+
+  mostRecentTime: Ember.computed('lastTimes', function() {
+    const lastTimes = this.get('lastTimes').slice();
+    return lastTimes.sort((a, b) => a - b).pop();
+  })
 
 });
--- tests/unit/models/event-test.js
+++ tests/unit/models/event-test.js
@@ -1,12 +1,30 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
+import moment from 'moment';
 
 moduleForModel('event', 'Unit | Model | event', {
   // Specify the other units that are required for this test.
   needs: []
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+test('retrieves the most recent time', function(assert) {
+  const first = moment();
+  const second = first.add(1, 's');
+
+  let model = this.subject({
+    lastTimes: Ember.A([ first, second ])
+  });
+
+  assert.strictEqual(model.get('mostRecentTime').valueOf(), second.valueOf());
+});
+
+test('retrieves the most recent time', function(assert) {
+  const first = moment();
+  const second = first.subtract(1, 's');
+
+  let model = this.subject({
+    lastTimes: Ember.A([ first, second ])
+  });
+
+  assert.strictEqual(model.get('mostRecentTime').valueOf(), first.valueOf());
 });
